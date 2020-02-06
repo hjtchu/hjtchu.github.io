3701d558f18abf8e0053d623a9aba0eef7c5aa20
Nguyễn Khang <nguyenkhangam1@gmail.com>
2020-02-06 21:25:26 +0700

    6.2.new

---
+++ Buổi 13/table/main.js
@@ -0,0 +1,17 @@
+var data = [
+    {firstName: 'Nguyen', lastName: 'Ha', email: 'nguyenha@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Hang', email: 'nguyenhang@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Hoang', email: 'nguyenhoang@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Hong', email: 'nguyenhong@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Hai', email: 'nguyenhai@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Nam', email: 'nguyennam@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Hue', email: 'nguyenhue@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Quy', email: 'nguyenquy@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Hung', email: 'nguyenhung@gmail.com'}
+    {firstName: 'Nguyen', lastName: 'Tuan', email: 'nguyentuan@gmail.com'}
+]
+
+function buildTable(data){
+    buildTable.innerHTML = '';
+    var row = data.map(e,i) => 
+}
