6d4dc95b8f6521a379f6b3676c0a9042e5d34b9a
Nguyễn Khang <nguyenkhangam1@gmail.com>
2020-02-20 17:39:26 +0700

    20/2

---
+++ Buoi_16/main.js
@@ -0,0 +1,32 @@
+var x = document.getElementById("main-menu1");
+
+var clickAction = document.getElementById('clickmenu');
+
+ clickAction.onclick = clickFunction;
+
+function showBlock(){
+  x.style.left = '0'
+  x.style.transitionDuration = '1.5s';
+ }
+
+function showNone(){
+  x.style.left = '-500px';
+  x.style.transitionDuration = '1.5s';
+
+
+}
+
+
+ 
+ function clickFunction() {
+  if(clickAction.dataset.order =='block'){
+    clickAction.dataset.order ='none';
+   showBlock();
+  } else {
+    clickAction.dataset.order = 'block';
+    showNone();
+  }
+
+ }
+
+ 
