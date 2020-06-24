144ae28a0be574e461550b3486a85e8d4c3f35c5
hjtchu <nguyenkhangam1@gmail.com>
2020-06-25 01:09:04 +0700

    25.6

--- Website-themes/main.js
+++ Website-themes/main.js
@@ -1,13 +1,1 @@
-var marker = document.querySelector('#marker');
-var item = document.querySelectorAll('nav a');
-
-
-function indicator (e){
-    marker.style.left = e.offsetLeft + "px";
-    marker.style.width = e.offsetWidth + "px";
-}
-item.forEach(hover => {
-    hover.addEventListener('mouseover', (e) => {
-        indicator(e.target);
-    })
-})
+// 
